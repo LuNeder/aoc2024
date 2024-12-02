57c2ed77240a1ef7d212952f10e224d88b791770
Luana <luana@luana.dev.br>
2024-12-02 16:56:02 -0300

    Challenge 1 Part two

--- src/1/1.f
+++ src/1/1.f
@@ -2,8 +2,8 @@
       contains
       ! Sorts a list in crescent order
       subroutine sort(col, a, lines)
-        real, dimension(:,:), allocatable :: a ! will allocate itself it seems
-        real aux
+        real*16, dimension(:,:), allocatable :: a ! will allocate itself it seems
+        real*16 aux
         integer lines, i, j, col
         do i = 1, lines
           do j = i+1, lines
@@ -20,9 +20,9 @@
       ! implicit none defined by fpm.toml. Not sure if I like using implicit none tbh (implicit declaration is interesting), but at least I can't misspell a var without it warning me...
       program one
         use sorting
-        real, dimension(:,:), allocatable :: lists
-        real totdist
-        integer lines, io, i
+        real*16, dimension(:,:), allocatable :: lists
+        real*16 totdist, simil
+        integer lines, io, i, j, num
 
         open(unit=12, file='input.txt')
         
@@ -54,5 +54,17 @@
         enddo
 
         write (*,*) totDist
+
+        ! Part Two 
+          ! real*16 because real (real*4?) gave me (rightAnswer-1), which is a wrong answer
+        simil = 0.0e0
+        do i = 1, lines
+          num = 0.0e0
+          do j = 1, lines
+            if (lists(1, i) == lists(2,j)) num = num + 1
+          end do
+          simil = (lists(1, i) * num) + simil
+        end do
+        write (*,*) 'Similarity: ', simil
       end program
       
