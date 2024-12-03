46bace35aafb730720ca8064d0a1bc01ed0ed1f8
Luana <luana@luana.dev.br>
2024-12-02 22:07:47 -0300

    2b FUCKING FINALLY, that was harder than it should, I'm probs just tired...

--- src/2/2.f
+++ src/2/2.f
@@ -1,46 +1,11 @@
-      program two
-        character*200, dimension(:), allocatable :: reports
-        real*16, dimension(:), allocatable :: report
-        integer lines, ncol, io, i, j, totSafe
-        logical unsafe, decreasing
-        open(unit=12, file='input.txt')
-        
-        ! Count number of items to allocate
-        io = 0
-        lines = 0
-        do while (io == 0)
-          read(12, *, iostat=io)
-          lines = lines + 1
-        end do
-        lines = lines - 1
-        allocate(reports(lines))
-        rewind(12) ! Start over from 1st line
-
-        ! Read each entire line (report) into a variable, as a string
-        read(12, '(A)') reports
-        close(12)
-
-        totSafe = 0
-        ! For each report in reports
-        do i = 1, lines
-
-          ! Count the number of columns (levels) and allocate 'report' to that
-          io = 0
-          ncol = 1
-          do while (io == 0)
-            if (allocated(report)) deallocate(report)
-            allocate(report(ncol))
-            read(reports(i), *, iostat=io) report
-            !write(*,*) report
-            ncol = ncol + 1
-          end do
-          ncol = ncol - 2 ! Why -2 and not -1? Why does it add an extra 0 at the end?
-          !write(*,*) 'ncol = ', ncol
-          if (allocated(report)) deallocate(report)
-          allocate(report(ncol))
-
-          ! Read each level of the report to the report vector, as a real*16
-          read(reports(i), *) report
+      module security
+      contains
+        subroutine safety(report, i, unsafe, ncol)
+          character*200, dimension(:), allocatable :: reports
+          real*16, dimension(:), allocatable :: report
+          real*16, dimension(:), allocatable :: reportnew
+          integer lines, ncol, io, i, j, totSafe
+          logical unsafe, decreasing
 
           unsafe = .FALSE.
           decreasing = .FALSE.
@@ -74,9 +39,81 @@
               unsafe = .TRUE.
             end if      
           end do
+          !write(*,*) 'Subr ', unsafe, ':', report
+        end subroutine
+      end module
+
+
+
+
+
+      program two
+        use security
+        character*200, dimension(:), allocatable :: reports
+        real*16, dimension(:), allocatable :: report
+        real*16, dimension(:), allocatable :: reportnew
+        integer lines, ncol, io, i, j, k, totSafe
+        logical unsafe, decreasing
+        open(unit=12, file='input.txt')
+        
+        ! Count number of items to allocate
+        io = 0
+        lines = 0
+        do while (io == 0)
+          read(12, *, iostat=io)
+          lines = lines + 1
+        end do
+        lines = lines - 1
+        allocate(reports(lines))
+        rewind(12) ! Start over from 1st line
+
+        ! Read each entire line (report) into a variable, as a string
+        read(12, '(A)') reports
+        close(12)
+
+        totSafe = 0
+        ! For each report in reports
+        do i = 1, lines
+          ! Count the number of columns (levels) and allocate 'report' to that
+          io = 0
+          ncol = 1
+          do while (io == 0)
+            if (allocated(report)) deallocate(report)
+            allocate(report(ncol))
+            read(reports(i), *, iostat=io) report
+            !write(*,*) report
+            ncol = ncol + 1
+          end do
+          ncol = ncol - 2 ! Why -2 and not -1? Why does it add an extra 0 at the end?
+          !write(*,*) 'ncol = ', ncol
+          if (allocated(report)) deallocate(report)
+          allocate(report(ncol))
+
+          ! Read each level of the report to the report vector, as a real*16
+          read(reports(i), *) report
+          call safety(report, i, unsafe, ncol)
           
           if (unsafe) then
             write(*,*) 'Unsafe: ', reports(i)
+
+            ! Part Two
+            if (allocated(reportnew)) deallocate(reportnew)
+            allocate(reportnew(ncol-1))
+            do j = 1, ncol
+              do k = 1, ncol
+                if (k < j) then 
+                  reportnew(k) = report(k)
+                else if (k > j) then 
+                  reportnew(k-1) = report(k)
+                end if
+              end do
+              call safety(reportnew, i, unsafe, ncol-1)
+              if (unsafe .eqv. .FALSE.) exit
+            end do
+            if (unsafe .eqv. .FALSE.) then
+              write(*,*) 'Fixed - Safe: ', reportnew
+              totSafe = totSafe + 1
+            end if
           else
             write(*,*) 'Safe: ', reports(i)
             totSafe = totSafe + 1
